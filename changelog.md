b2c78c25ea555f0ab32e272a92d0b912bd850ede
Ben Braun <ben.braun-1@colorado.edu>
2024-12-11 00:11:31 +0000

    Version number

--- changelog.md
+++ changelog.md
@@ -1,4 +1,4 @@
-### 0.1.3
+### 0.1.4
 - Added an RPM threshold to HMO gene detection (default set to 10)
 - Made a few clean output tables to be used in downstream analysis
 - Cleaned up comments
--- src/bifidotyper/__init__.py
+++ src/bifidotyper/__init__.py
@@ -2,6 +2,6 @@ from .cli import main
 from .processor import build_sample_dict
 from .references import ReferenceManager
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __all__ = ["main", "build_sample_dict", "ReferenceManager","SylphUtils","HMOUtils","PlotUtils","logger"]
